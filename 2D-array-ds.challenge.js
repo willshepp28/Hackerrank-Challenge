b3f13dc234ce5d8173ddc8cee714c867b4e7e11f
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-11-13 18:58:14 -0800

    initial commit

---
+++ 2D-array-ds.challenge.js
@@ -0,0 +1,76 @@
+let testArr = [
+    [1,5,3], // start
+    [4,8,7], // end 
+     [6,9,1] //  summand
+];
+
+let testArr2 = [
+    [1,2,100],
+    [2,5,100],
+    [3,4,100]
+];
+
+
+// creates the new array with based on the given number of indicies
+function createArray(numberOfIndices){
+
+    if(!numberOfIndices) {
+        console.log("please enter a value");
+        return;
+    }
+
+    if(typeof numberOfIndices !== "number") {
+        console.log("Please enter a number");
+        return;
+    }
+
+    let arr = [];
+
+    for(let i = 0; i < numberOfIndices; i++) {
+        arr.push(0)
+    }
+
+    return arr;
+
+};
+
+ 
+
+
+
+
+
+
+function manipulateArray(n, queries) {
+    let arr = createArray(n);
+    let largestNumber = 0;
+
+    for(let i = 0; i < queries.length; i++) {
+        
+        console.log(i)
+        for(let j = 0; j < arr.length; j++) {
+            let index = j + 1;
+            
+            let start = queries[i][0];
+            let stop = queries[i][1];
+            let sumToAdd = queries[i][2];
+
+            //  if the current index is greater than or equal to  the start, or less than or equal to stop add sum 
+            if(index >= start && index <= stop){
+                arr.splice(j, 1, arr[j] + sumToAdd)
+            }
+
+            // check if the newly added number is now the largest
+            if(arr[j] > largestNumber) {
+                largestNumber = arr[j];
+            }
+
+        }
+    
+    };
+
+    return largestNumber;
+};
+
+
+manipulateArray(10, testArr);
---
+++ array-ds.challenge.js
@@ -0,0 +1,58 @@
+/*
+    An array is a type of data structure that stores elements of the same type in a contiguous block of memory. In an array, , of size , each memory location has some unique index,  (where ), that can be referenced as  or .
+    Reverse an array of integers.
+    Note: If you've already solved our C++ domain's Arrays Introduction challenge, you may want to skip this.
+    Example
+    Return .
+    Function Description
+    Complete the function reverseArray in the editor below.
+    reverseArray has the following parameter(s):
+    int A[n]: the array to reverse
+    Returns
+    int[n]: the reversed array
+*/
+
+
+let testArray1 = [1,2,3];
+let testArray2 = [6,7,8];
+
+
+
+
+// Write a function that reverse array with unshift
+// Note: unshift has a time complexity of 0(n)
+function reverseArrayUsingUnshift(a) {
+    let newArr = [];
+
+    for(let i = 0; i < a.length; i++) {
+        newArr.unshift(a[i]);
+    }
+
+    return newArr;
+}
+
+
+
+
+/* 
+Write a function that reverses array with pop method
+    Note: Array.pop() has a time complexity of 0(1)
+    1. Initialize a new array
+    2. Iterate over that passed in array, starting from that last index of that array
+    3. Push every item in the passed in array, started from the last index into the new array
+    4. Return the new array
+ 
+ */
+function reverseArrayUsingPop(a){
+    let newArr = [];
+    
+    for(let i = 0; i < a.length; i++) {
+        newArr.push(a[a.length - i - 1]);
+    }
+
+    return newArr;
+};
+
+
+reverseArrayUsingUnshift(testArray1);
+reverseArrayUsingPop(testArray2);
